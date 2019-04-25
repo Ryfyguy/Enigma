0ce044706f09f869225250a8cba33ec8770fb29b
Jackie Yang <jy532@drexel.edu>
2019-04-25 16:30:05 -0400

    Maze

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float WalkSpeed;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            float WalkTranslation = Input.GetAxis("Horizontal") * Time.deltaTime * WalkSpeed;
+            transform.Translate(WalkTranslation, 0, 0);
+        }
+        if (Input.GetButton("Vertical"))
+        {
+            float WalkTranslation = Input.GetAxis("Vertical") * WalkSpeed;
+            WalkTranslation *= Time.deltaTime;
+            transform.Translate(0, WalkTranslation, 0);
+        }
+    }
+}
