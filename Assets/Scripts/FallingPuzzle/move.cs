198dfbbcdc4ff6e221b7e1c9d7078c04ac412a56
Danny Campbell <dtc53@drexel.edu>
2019-05-23 15:15:04 -0400

    Danny's Stuff

---
+++ Assets/Scripts/FallingPuzzle/move.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float Speed;
+    public float Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "object")
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    void Update()
+    {
+        var playerObject = GameObject.Find("Player");
+        var playerPos = playerObject.transform.position;
+
+        Timer -= Time.deltaTime;
+
+        if (Input.GetButton("Horizontal"))
+        {
+            float WalkTranslation = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+            transform.Translate(WalkTranslation, 0, 0);
+        }
+
+        if (playerPos[1] <= -24)
+        {
+            playerPos[1] = 24;
+            playerObject.transform.position = playerPos;
+        }
+
+        if (Timer < 0)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
